dba0974d98135ff1bb2b9d077712a835880b1a76
Daniel-Martinez24 <dan24diaz@gmail.com>
2022-10-02 21:56:40 -0500

    playfair

--- main.py
+++ main.py
@@ -54,6 +54,90 @@ def otp_desencoder(msj: str, key: str) -> str:
 
     return ''.join(msj_encript) #Se añaden a string las letras del nuevo msj encriptado
 
+# código playfair
+list1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 
+        'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def playflair_menu(msj:str, option:int) -> str:
+    while True:
+        key = input('Qué llave deseas utilizar? ').upper()
+        if (key.isalpha()):
+            break
+        else:
+            print('solo letras en la clave')
+    return playfair(key, msj) if option == 1 else playfair(key, msj, encrypt=False)
+    
+def create_matrix(key):
+    key = key.upper()
+    matrix = [[0 for i in range (5)] for j in range(5)]
+    letters_added = []
+    row = 0
+    col = 0
+    for letter in key:
+        if letter not in letters_added:
+            matrix[row][col] = letter
+            letters_added.append(letter)
+        else:
+            continue
+        if (col==4):
+            col = 0
+            row += 1
+        else:
+            col += 1
+    for letter in range(65,91):
+        if letter==74:
+                continue
+        if chr(letter) not in letters_added:
+            letters_added.append(chr(letter))
+    index = 0
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = letters_added[index]
+            index+=1
+    return matrix
+
+def separate_same_letters(message):
+    index = 0
+    while (index<len(message)):
+        l1 = message[index]
+        if index == len(message)-1:
+            message = message + 'X'
+            index += 2
+            continue
+        l2 = message[index+1]
+        if l1==l2:
+            message = message[:index+1] + "X" + message[index+1:]
+        index +=2   
+    return message
+
+def indexOf(letter,matrix):
+    for i in range (5):
+        try:
+            index = matrix[i].index(letter)
+            return (i,index)
+        except:
+            continue
+def playfair(key, message, encrypt=True):
+    inc = 1
+    if encrypt==False:
+        inc = -1
+    matrix = create_matrix(key)
+    message = message.upper()
+    message = message.replace(' ','')    
+    message = separate_same_letters(message)
+    cipher_text=''
+    for (l1, l2) in zip(message[0::2], message[1::2]):
+        row1,col1 = indexOf(l1,matrix)
+        row2,col2 = indexOf(l2,matrix)
+        if row1==row2: #Rule 2, the letters are in the same row
+            cipher_text += matrix[row1][(col1+inc)%5] + matrix[row2][(col2+inc)%5]
+        elif col1==col2:# Rule 3, the letters are in the same column
+            cipher_text += matrix[(row1+inc)%5][col1] + matrix[(row2+inc)%5][col2]
+        else: #Rule 4, the letters are in a different row and column
+            cipher_text += matrix[row1][col2] + matrix[row2][col1]
+    
+    return cipher_text
+
 # codigo Hill
 mat_cif = np.array([[1,2,3], [0,4,5], [1,0,6]])
 mat_cif_t = np.array([[6, 24, 22] , [26, 21, 1], [17, 5, 10]])
@@ -175,6 +259,10 @@ def main() -> None:
                 print('La palabra y la llave deben tener el mismo número de letras')
 
         print(otp_menu(option=temp, msj=msj, key=key))
+    
+    if (typ == 1):
+        print(playflair_menu(option=temp, msj=msj))
+
 
 if __name__ == '__main__':
     main()
